beb45bc3ce39baaef97031cc33b3761f319f3ab5
ISMAA'EL MUHAMMAD IBRAHIM Khodabucchas <24378437@sun.ac.za>
2022-10-10 02:16:10 +0200

    please

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import RouterDOM from './Router';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  // <React.StrictMode>
     <RouterDOM />,
-  </React.StrictMode>,
+  // </React.StrictMode>,
+  document.getElementById('root')
 );

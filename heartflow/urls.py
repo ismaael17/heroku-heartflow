6ba6e58ca7bcc7a1e485089d650ca60379a894fc
ISMAA'EL MUHAMMAD IBRAHIM Khodabucchas <24378437@sun.ac.za>
2022-10-12 19:13:53 +0200

    urls

--- heartflow/urls.py
+++ heartflow/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    re_path(r'^', include('users.urls')),
+    re_path(r'^', include('volunteers.urls')),
 ]

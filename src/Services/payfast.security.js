36d128e9036839f536d9c83c632951cde0a0f6c2
ISMAA'EL MUHAMMAD IBRAHIM Khodabucchas <24378437@sun.ac.za>
2022-10-12 21:40:43 +0200

    changed from localhost to website's url

--- src/Services/payfast.security.js
+++ src/Services/payfast.security.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/'
+const API_URL = 'https://heartflow-support-system.herokuapp.com/'
 
 
 class PayfastSecurity {
--- src/Services/volunteers.service.js
+++ src/Services/volunteers.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {config} from "process";
 
-const API_URL = 'http://127.0.0.1:8000/'
+const API_URL = 'https://heartflow-support-system.herokuapp.com/'
 
 class VolunteersService {
     getOnlineOrders(token, status) {

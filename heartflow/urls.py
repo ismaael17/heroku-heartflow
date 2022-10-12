a8ab7e1c59fb8d3ed2510a8af1022a03aa32ef81
ISMAA'EL MUHAMMAD IBRAHIM Khodabucchas <24378437@sun.ac.za>
2022-10-12 22:35:44 +0200

    urls again

--- heartflow/urls.py
+++ heartflow/urls.py
@@ -20,9 +20,9 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-
     re_path(r'^', include('users.urls')),
     re_path(r'^', include('volunteers.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
+
 ]
